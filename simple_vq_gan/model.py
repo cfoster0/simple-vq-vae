947d93350896657e4940b46d9e688652dcda7586
Charles Foster <cfoster0@stanford.edu>
2021-06-25 23:54:44 -0700

    Encoder and decoder classes

--- simple_vq_gan/model.py
+++ simple_vq_gan/model.py
@@ -1,5 +1,19 @@
 import torch
-import torch.nn as NN
+import torch.nn as nn
 import torch.nn.functional as F
 
 from einops import rearrange, repeat, reduce
+
+class Encoder(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
+
+class Decoder(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass

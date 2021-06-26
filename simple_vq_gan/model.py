7a72722d8b7fddfcfe0eaa3e4fc3db4da4c9b22b
Charles Foster <cfoster0@stanford.edu>
2021-06-25 23:58:51 -0700

    Other classes

--- simple_vq_gan/model.py
+++ simple_vq_gan/model.py
@@ -4,6 +4,27 @@ import torch.nn.functional as F
 
 from einops import rearrange, repeat, reduce
 
+class Quantize(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
+
+class Residual(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
+
+class Block(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
+
 class Encoder(nn.Module):
     def __init__(self):
         pass

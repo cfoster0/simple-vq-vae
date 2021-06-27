51109ff6e469c68a3284efbac3c83a4a2a095047
Charles Foster <cfoster0@stanford.edu>
2021-06-26 17:30:55 -0700

    Linear interpolation

--- simple_vq_gan/model.py
+++ simple_vq_gan/model.py
@@ -54,7 +54,7 @@ class Block(nn.Module):
                                    self.hidden_dim,
                                    self.hidden_dim,
                                    ], -1)
-        (k, v) = map(lambda x: F.interpolate(x, scale_factor=self.compression), (k, v))
+        (k, v) = map(lambda x: F.interpolate(x, scale_factor=self.compression, mode='linear'), (k, v))
         (q, k, v) = map(lambda x: x.transpose(self.axis, -2), (q, k, v))
         (q, k, v) = map(lambda x: rearrange(x, "... (h d) -> ... h d", h=self.heads), (q, k, v))
         (q, k) = map(lambda x: self.rotary(x), (q, k))
